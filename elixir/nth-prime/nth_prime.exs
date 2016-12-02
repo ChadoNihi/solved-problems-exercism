c311a885f1aa37f321c4b68d927df99db466dffd
ChadoNihi <chadoishere@gmail.com>
2016-12-02 19:01:36 +0200

    nth prime elixir v1

--- elixir/nth-prime/nth_prime.exs
+++ elixir/nth-prime/nth_prime.exs
@@ -1,26 +1,42 @@
 defmodule Prime do
-
+  require Integer
   @doc """
   Generates the nth prime.
   """
-  fstFivePrimes = [2,3,5,7,11]
+  @fstFivePrimes [2,3,5,7,11,13]
 
   @spec nth(non_neg_integer) :: non_neg_integer
+  def nth(n) when n <=0 do
+    raise "invalid number"
+  end
   def nth(n) do
-    if n < 6 do
-      Enum.at(fstFivePrimes, n-1)
+    if n < 7 do
+      Enum.at(@fstFivePrimes, n-1)
     else
-      startingCand = round(n*ln(n) + n*(ln(ln(n))-1))
-      findPrimeStartingFrom( if Integer.is_even(startingCand), do: startingCand+1, else: startingCand )
+      startingCand = round(n*:math.log(n) + n*(:math.log(:math.log(n))-1))
+      findPrimeStartingFromOdd( if Integer.is_even(startingCand), do: startingCand+1, else: startingCand )
     end
   end
 
-  defp findPrimeStartingFrom(cand) do
-    
+  defp findPrimeStartingFromOdd(cand) do
+    if isPrime(cand) do
+      cand
+    else
+      findPrimeStartingFromOdd(cand+2)
+    end
   end
 
-  defp ln(x) do
-    :math.log10(x) * :math.log10(2.718281828459045)
-    |> round
+  defp isPrime(x) do
+    Integer.is_odd(x) and
+    testFactors(3, x, x)
+  end
+
+  defp testFactors(f, bound, pr) when f > bound, do: true
+  defp testFactors(f, bound, pr) do
+    if rem(pr, f) == 0 do
+      false
+    else
+      testFactors(f+2, bound, pr)
+    end
   end
 end
