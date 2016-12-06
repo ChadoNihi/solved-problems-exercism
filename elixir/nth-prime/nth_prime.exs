aa51f1b62f53b124af2fdc21d4b574e069a86dab
ChadoNihi <chadoishere@gmail.com>
2016-12-06 19:31:05 +0200

    nth_prime elixir

--- elixir/nth-prime/nth_prime.exs
+++ elixir/nth-prime/nth_prime.exs
@@ -3,51 +3,32 @@ defmodule Prime do
   @doc """
   Generates the nth prime.
   """
-  @fstFivePrimes [2,3,5,7,11,13]
-
   @spec nth(non_neg_integer) :: non_neg_integer
   def nth(n) when n <=0 do
     raise "invalid number"
   end
   def nth(1), do: 2
   def nth(n) do
-    upTo = Float.ceil(n*:math.log(n) + n*(:math.log(:math.log(n))))
-
+    _nth(n, 1, 3)
   end
 
-
-
-  def nth(n) when n <=0 do
-    raise "invalid number"
-  end
-  def nth(n) do
-    if n < 7 do
-      Enum.at(@fstFivePrimes, n-1)
+  defp _nth(n, n, cand) , do: cand-2
+  defp _nth(n, cnt, cand) do
+    if is_prime(cand) do
+      _nth(n, cnt+1, cand+2)
     else
-      startingCand = round(n*:math.log(n) + n*(:math.log(:math.log(n))-1))
-      findPrimeStartingFromOdd( if Integer.is_even(startingCand), do: startingCand+1, else: startingCand )
+      _nth(n, cnt, cand+2)
     end
   end
 
-  defp findPrimeStartingFromOdd(cand) do
-    if isPrime(cand) do
-      cand
-    else
-      findPrimeStartingFromOdd(cand+2)
-    end
-  end
+  defp is_prime(x), do: is_prime(x, 3, Float.floor(:math.sqrt(x)))
 
-  defp isPrime(x) do
-    Integer.is_odd(x) and
-    testFactors(3, x, x)
-  end
-
-  defp testFactors(f, bound, pr) when f > bound, do: true
-  defp testFactors(f, bound, pr) do
-    if rem(pr, f) == 0 do
+  defp is_prime(_, cand_factor, till) when cand_factor > till, do: true
+  defp is_prime(x, cand_factor, till) do
+    if rem(x, cand_factor) == 0 do
       false
     else
-      testFactors(f+2, bound, pr)
+      is_prime(x, cand_factor+2, till)
     end
   end
 end
