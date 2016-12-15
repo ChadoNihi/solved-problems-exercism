79cd60bd114d38d43c0f9d41a89bf36bbc009075
ChadoNihi <chadoishere@gmail.com>
2016-12-15 18:12:04 +0200

    elixir grade-school done

--- elixir/grade-school/school.exs
+++ elixir/grade-school/school.exs
@@ -10,7 +10,7 @@ defmodule School do
   """
   @spec add(map, String.t, integer) :: map
   def add(db, name, grade) do
-
+    Map.update(db, grade, [name], fn(names) -> names ++ [name] end)
   end
 
   @doc """
@@ -18,7 +18,7 @@ defmodule School do
   """
   @spec grade(map, integer) :: [String.t]
   def grade(db, grade) do
-
+    Map.get(db, grade, [])
   end
 
   @doc """
@@ -26,6 +26,7 @@ defmodule School do
   """
   @spec sort(map) :: [{integer, [String.t]}]
   def sort(db) do
-
+    Enum.map(db, fn {grade, names} -> {grade, Enum.sort(names)} end)
+    |> Enum.sort(fn {grade1, _}, {grade2, _} -> grade1<=grade2 end)
   end
 end
