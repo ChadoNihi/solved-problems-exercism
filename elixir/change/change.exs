57a9abe2f569c6f4c617843c90f3ccd57d06b8d8
ChadoNihi <chadoishere@gmail.com>
2016-12-26 18:34:10 +0200

    elixir change done

--- elixir/change/change.exs
+++ elixir/change/change.exs
@@ -17,12 +17,28 @@ defmodule Change do
 
   @spec generate(integer, list) :: {:ok, map} | :error
   def generate(amount, values) do
-    coins = Enum.filter(values, &(&1 <= amount))
+    coin_types = Enum.sort(values, &(&1 > &2))
 
-    if Enum.empty?(coins) or Enum.min(coins) > amount do
+    if Enum.empty?(coin_types) or Enum.min(coin_types) > amount do
       :error
     else
+      coins = Map.new(coin_types, &{&1, 0})
+      min_change(amount, coin_types, coins)
+    end
+  end
 
+  defp min_change(0, _, coins), do: {:ok, coins}
+  defp min_change(_, [], _), do: :error
+  defp min_change(amount, [coin | rest], coins) when coin > amount do
+    min_change(amount, rest, coins)
+  end
+  defp min_change(amount, [coin | rest] = coin_types, coins) do
+    rem = amount - coin
+
+    if Enum.any?(coin_types, &(rem(rem, &1)==0)) do
+      min_change(rem, coin_types, Map.update!(coins, coin, &(&1 + 1)))
+    else
+      min_change(amount, rest, coins)
     end
   end
 end
