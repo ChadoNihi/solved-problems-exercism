8d593cdd630043eacd3a9d49a813b37041ee129b
ChadoNihi <chadoishere@gmail.com>
2016-12-26 18:40:22 +0200

    elixir parallel-letter-frequency started

---
+++ elixir/parallel-letter-frequency/frequency.exs
@@ -0,0 +1,14 @@
+defmodule Frequency do
+  @doc """
+  Count letter frequency in parallel.
+
+  Returns a map of characters to frequencies.
+
+  The number of worker processes to use can be set with 'workers'.
+  """
+  @spec frequency([String.t], pos_integer) :: map
+  def frequency([], _), do: %{}
+  def frequency(texts, workers) do
+
+  end
+end
