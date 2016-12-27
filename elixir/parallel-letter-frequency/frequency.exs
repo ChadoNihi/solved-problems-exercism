da5fa0458a919f7cc6fe8515cc048e0267555fac
ChadoNihi <chadoishere@gmail.com>
2016-12-27 19:26:05 +0200

    save

--- elixir/parallel-letter-frequency/frequency.exs
+++ elixir/parallel-letter-frequency/frequency.exs
@@ -9,6 +9,19 @@ defmodule Frequency do
   @spec frequency([String.t], pos_integer) :: map
   def frequency([], _), do: %{}
   def frequency(texts, workers) do
+    text = Enum.join(texts)
 
+    if String.length(text) >= workers do
+      String.graphemes(text)
+      |> Enum.chunk(workers)
+      |> Enum.map(&Task.async(&(get_letter_freqs(&1))))
+      |> Enum.map(&Task.await(&1))
+    else
+
+    end
+  end
+
+  defp get_letter_freqs(l) do
+    Enum.reduce(l, %{}, &( Map.update ))
   end
 end
