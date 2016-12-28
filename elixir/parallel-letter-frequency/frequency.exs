01ae8dbcda4c1e26a2f65133736cc5a9f049d801
ChadoNihi <chadoishere@gmail.com>
2016-12-28 17:25:51 +0200

    elixir parallel-letter-freqs done

--- elixir/parallel-letter-frequency/frequency.exs
+++ elixir/parallel-letter-frequency/frequency.exs
@@ -9,19 +9,24 @@ defmodule Frequency do
   @spec frequency([String.t], pos_integer) :: map
   def frequency([], _), do: %{}
   def frequency(texts, workers) do
-    text = Enum.join(texts)
+    l_of_all_chars = Enum.join(texts)
+    |> String.downcase
+    |> String.graphemes
 
-    if String.length(text) >= workers do
-      String.graphemes(text)
-      |> Enum.chunk(workers)
-      |> Enum.map(&Task.async(&(get_letter_freqs(&1))))
+    if Enum.count(l_of_all_chars) >= workers do
+      Enum.chunk(l_of_all_chars, workers)
+      |> Enum.map(&Task.async(fn() -> get_letter_freqs(&1) end))
       |> Enum.map(&Task.await(&1))
+      |> Enum.reduce(fn(map, acc) -> Map.merge(acc, map, fn(_, v1, v2) -> v1+v2 end) end)
     else
-
+      get_letter_freqs(l_of_all_chars)
     end
   end
 
+  @pattern ~r/\w/u
+  @pattern_digit ~r/\d/
   defp get_letter_freqs(l) do
-    Enum.reduce(l, %{}, &( Map.update ))
+    Enum.filter(l, fn(ch) -> String.match?(ch, @pattern) and ! String.match?(ch, @pattern_digit) end)
+    |> Enum.reduce(%{}, fn(ch, acc) -> Map.update(acc, ch, 1, &(&1+1)) end)
   end
 end
