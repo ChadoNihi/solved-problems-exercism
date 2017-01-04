118aed2ddd35013edc1f6408fe2c9e255677409b
ChadoNihi <chadoishere@gmail.com>
2017-01-04 17:55:31 +0200

    elixir pascals-triangle done

--- elixir/pascals-triangle/pascals_triangle.exs
+++ elixir/pascals-triangle/pascals_triangle.exs
@@ -6,7 +6,18 @@ defmodule PascalsTriangle do
   @spec rows(integer) :: [[integer]]
   def rows(n) when n<=0, do: :error
   def rows(0), do: []
+  def rows(1), do: [[1]]
+  def rows(2), do: [[1], [1,1]]
   def rows(n) do
-    
+    Enum.reduce(3..n, [[1,1], [1]], fn(r, [h | _t] = acc) ->
+      [make_row(h, r) | acc]
+    end)
+    |> Enum.reverse
+  end
+
+  defp make_row(prev_r, curr_r_num) do
+    Enum.reduce(1..(curr_r_num-2), [1], fn(i, acc) -> [Enum.at(prev_r, i)+Enum.at(prev_r, i-1) | acc] end)
+    |> Enum.reverse
+    |> Kernel.++ [1]
   end
 end
