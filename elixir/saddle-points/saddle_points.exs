9906d988d1b9a5b8ffe739aaa161529f6b182338
ChadoNihi <chadoishere@gmail.com>
2017-01-05 11:17:35 +0200

    save

---
+++ elixir/saddle-points/saddle_points.exs
@@ -0,0 +1,32 @@
+defmodule Matrix do
+  @doc """
+  Parses a string representation of a matrix
+  to a list of rows
+  """
+  @spec rows(String.t()) :: [[integer]]
+  def rows(str) do
+    String.split(str, "\n")
+    |> Enum.map(fn(row_str) ->
+      String.split(row_str, " ") |> Enum.map(&(String.to_integer(&1)))
+    end)
+  end
+
+  @doc """
+  Parses a string representation of a matrix
+  to a list of columns
+  """
+  @spec columns(String.t()) :: [[integer]]
+  def columns(str) do
+    rows(str)
+    |> Enum.reduce(fn(row) ->  end)
+  end
+
+  @doc """
+  Calculates all the saddle points from a string
+  representation of a matrix
+  """
+  @spec saddle_points(String.t()) :: [{integer, integer}]
+  def saddle_points(str) do
+
+  end
+end
