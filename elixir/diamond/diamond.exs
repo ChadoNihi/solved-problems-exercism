06551b00c9dbe31f895a977be2b0d7226a55e142
ChadoNihi <chadoishere@gmail.com>
2017-01-11 18:15:07 +0200

    elixir diamond done

--- elixir/diamond/diamond.exs
+++ elixir/diamond/diamond.exs
@@ -7,15 +7,52 @@ defmodule Diamond do
   def build_shape(ch) when ch >= ?a and ch <= ?z, do: build_shape(ch - (?a-?A))
   def build_shape(ch) when ch < ?A or ch > ?Z, do: raise ArgumentError
   def build_shape(max_ch) do
-    Enum.reduce(max_ch..?A, [], fn(ch, acc) -> [make_row(ch, max_ch-ch) | acc] end)
+    half = Enum.reduce(max_ch..?A, [], fn(ch, acc) -> [make_row(ch, max_ch-ch) | acc] end)
+    half ++ (Enum.reverse(half) |> tl)
+    |> Enum.join
   end
 
   defp make_row(ch, n_outer) do
     outer_spaces = List.duplicate(" ", n_outer)
-    [outer_spaces | [ch | [List.duplicate(" ", ch-?A) | [ch | outer_spaces]]]]
+    ch_str = ch_to_str(ch)
+
+    if ch === ?A do
+      [outer_spaces | [ch_str | outer_spaces++["\n"]]]
+    else
+      [outer_spaces | [ch_str | [List.duplicate(" ", (ch-?A)*2-1) | [ch_str | outer_spaces++["\n"]]]]]
+    end
+    |> Enum.join
   end
 
-  defp ch_to_ do
-
+  defp ch_to_str(ch) do
+    case ch do
+      ?A -> "A"
+      ?B -> "B"
+      ?C -> "C"
+      ?D -> "D"
+      ?E -> "E"
+      ?F -> "F"
+      ?G -> "G"
+      ?H -> "H"
+      ?I -> "I"
+      ?J -> "J"
+      ?K -> "K"
+      ?L -> "L"
+      ?M -> "M"
+      ?N -> "N"
+      ?O -> "O"
+      ?P -> "P"
+      ?Q -> "Q"
+      ?R -> "R"
+      ?S -> "S"
+      ?T -> "T"
+      ?U -> "U"
+      ?V -> "V"
+      ?W -> "W"
+      ?X -> "X"
+      ?Y -> "Y"
+      ?Z -> "Z"
+      true -> "?"
+    end
   end
 end
