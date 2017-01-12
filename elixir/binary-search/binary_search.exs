aab10282d7ba77d3452fcf75534c666b2f41f384
ChadoNihi <chadoishere@gmail.com>
2017-01-12 19:15:57 +0200

    elixir binary_search done

--- elixir/binary-search/binary_search.exs
+++ elixir/binary-search/binary_search.exs
@@ -21,8 +21,20 @@ defmodule BinarySearch do
   def search(nums, num) do
     search(nums, num, 0, tuple_size(nums)-1)
   end
+  def search(nums, num, min_i, max_i) when min_i === max_i do
+    if elem(nums, min_i) === num, do: {:ok, min_i}, else: :not_found
+  end
   def search(_nums, _num, min_i, max_i) when min_i > max_i, do: :not_found
   def search(nums, num, min_i, max_i) do
-
+    mid_i = min_i + (max_i - min_i |> div(2))
+    el = elem(nums, mid_i)
+    cond do
+      el === num ->
+        {:ok, mid_i}
+      num < el ->
+        search(nums, num, min_i, mid_i)
+      true ->
+        search(nums, num, mid_i+1, max_i)
+    end
   end
 end
