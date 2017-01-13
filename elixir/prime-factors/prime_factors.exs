dc3907f2100560f8f7464cdf87185cd0429b0005
ChadoNihi <chadoishere@gmail.com>
2017-01-13 19:29:24 +0200

    continue elixir prime_factors

--- elixir/prime-factors/prime_factors.exs
+++ elixir/prime-factors/prime_factors.exs
@@ -9,6 +9,46 @@ defmodule PrimeFactors do
   """
   @spec factors_for(pos_integer) :: [pos_integer]
   def factors_for(number) do
-    Enum.reduce_while(primes, [], fn(pr, fs) ->  end)
+    Enum.reduce_while(primes_up_to(:math.sqrt(number) |> round), %{fs: [], num: number}, fn(pr, acc) ->
+      {prs, num} = process_cand_fc(pr, number)
+      if num === 1 do
+        {:halt, Enum.concat(Map.get(acc, :fs), prs)}
+      else
+        {:cont, Map.update!(acc, :fs, &(Enum.concat(&1, prs)))
+          |> Map.put(:num, num)
+        }
+      end
+    end)
+  end
+
+  defp process_cand_fc(pr, num, prs \\ []) do
+    if rem(num, pr) === 0 do
+      process_cand_fc(pr, div(num, pr), [pr | prs])
+    else
+      {prs, num}
+    end
+  end
+
+  defp primes_up_to(max) do
+    primes_up_to(max, [2], 3)
+  end
+  defp primes_up_to(max, _, _) when max < 2, do: []
+  defp primes_up_to(max, prs, cand) when max < cand, do: Enum.reverse(prs)
+  defp primes_up_to(max, prs, cand) do
+    if is_prime(cand) do
+      primes_up_to(max, [cand | prs], cand+2)
+    else
+      primes_up_to(max, prs, cand+2)
+    end
+  end
+
+  defp is_prime(x), do: is_prime(x, 3, Float.floor(:math.sqrt(x)))
+  defp is_prime(_, cand_factor, till) when cand_factor > till, do: true
+  defp is_prime(x, cand_factor, till) do
+    if rem(x, cand_factor) == 0 do
+      false
+    else
+      is_prime(x, cand_factor+2, till)
+    end
   end
 end
