9b806a0f4554ab59eb20caa2aa333c8e6db0a147
ChadoNihi <chadoishere@gmail.com>
2017-01-24 12:23:07 +0200

    elixir crypto-square done

--- elixir/crypto-square/crypto_square.exs
+++ elixir/crypto-square/crypto_square.exs
@@ -7,16 +7,24 @@ defmodule CryptoSquare do
     "ac bd"
   """
   @spec encode(String.t) :: String.t
+  def encode(""), do: ""
   def encode(str) do
     norm_letters = String.downcase(str)
     |> String.graphemes
-    |> Enum.filter(&(String.match?(&1, ~r/^[a-z]$/)))
+    |> Enum.filter(&(String.match?(&1, ~r/^[a-z0-9]$/)))
 
-    row_sz = :math.sqrt(length(norm_letters)) |> Float.ceil |> round
+    n_cols = :math.sqrt(length(norm_letters)) |> Float.ceil |> round
 
-    Enum.chunk(norm_letters, row_sz, row_sz, [])
-    |> Enum.intersperse(" ")
+    Enum.chunk(norm_letters, n_cols, n_cols, List.duplicate(" ", n_cols))
+    |> Enum.zip
+    |> List.foldr([], fn(code_row, acc) ->
+      [
+        Tuple.to_list(code_row) |
+        [(if elem(code_row, tuple_size(code_row)-1)==" ", do: "", else: " ") | acc]
+      ]
+    end)
     |> List.flatten
     |> Enum.join
+    |> String.trim_trailing
   end
 end
