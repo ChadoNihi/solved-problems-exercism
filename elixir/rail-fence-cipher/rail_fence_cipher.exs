361feef1292d8259a097f4605c8c195035ebca3f
ChadoNihi <chadoishere@gmail.com>
2017-01-31 20:49:39 +0200

    save

--- elixir/rail-fence-cipher/rail_fence_cipher.exs
+++ elixir/rail-fence-cipher/rail_fence_cipher.exs
@@ -1,4 +1,5 @@
 defmodule RailFenceCipher do
+  require Integer
   @doc """
   Encode a given plaintext to the corresponding rail fence ciphertext
   """
@@ -6,11 +7,18 @@ defmodule RailFenceCipher do
   def encode(str, rails) do
     rows_map = String.graphemes(str)
     |> Enum.with_index
+    |> IO.inspect
     |> List.foldr(%{}, fn({gr, i}, rows) ->
-      Map.update(rows, rem(i, rails*2 - 2), [gr], fn(row) -> [gr | row] end)
+      Map.update(
+        rows,
+        (if rem(i, rails) == 0 and Integer.is_odd(div(i, rails)), do: rails - rem(i, rails)-1, else: rem(i, rails)),
+        [gr],
+        fn(row) -> [gr | row] end
+      )
     end)
 
     Enum.map(0..(rails-1), &(rows_map[&1]))
+    |> IO.inspect
     |> List.flatten
     |> Enum.join
   end
