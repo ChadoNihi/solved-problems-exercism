12a2155f3601d786c5dd3c650ad8ffb37e2d7ffc
ChadoNihi <chadoishere@gmail.com>
2017-02-25 19:59:24 +0200

    save

--- elixir/minesweeper/minesweeper.exs
+++ elixir/minesweeper/minesweeper.exs
@@ -6,12 +6,19 @@ defmodule Minesweeper do
   @spec annotate([String.t]) :: [String.t]
 
   def annotate(board) do
-    Enum.map(board, fn(row_str) ->
+    max_r = Enum.count(board)-1
+    max_c = String.length(hd(board))
+    brd = Enum.map(board, fn(row_str) ->
       String.graphemes(row_str)
     end)
-    |> Enum.map(fn(row) ->
-      Enum.reduce(row, %{prev: 0, curr: , next: , acc_row: []}, fn(x, mem) ->
 
+    Enum.map(0..max_r, fn(r) ->
+      Enum.reduce(max_c..0, %{left: , center: , right: 0, acc_row: []}, fn(x, mem) ->
+        if x == "*" do
+          Map.update!(mem, :acc_row, &([x | &1]))
+        else
+
+        end
       end)
     end)
   end
