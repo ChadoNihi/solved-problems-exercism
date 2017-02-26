9fd8fc9b7a3e3c4f5a3650d3f09e887ee3eebfc8
ChadoNihi <chadoishere@gmail.com>
2017-02-26 21:26:44 +0200

    save

--- elixir/minesweeper/minesweeper.exs
+++ elixir/minesweeper/minesweeper.exs
@@ -8,18 +8,35 @@ defmodule Minesweeper do
   def annotate(board) do
     max_r = Enum.count(board)-1
     max_c = String.length(hd(board))
-    brd = Enum.map(board, fn(row_str) ->
-      String.graphemes(row_str)
+    brd = Enum.reduce(board, {0, %{}}, fn(row_str, {r, row}) ->
+      {
+        r+1,
+        Map.put(row, r,
+          String.graphemes(row_str)
+          |> Enum.reduce({0, %{}}, fn(cell, {c, }) -> ... end)
+          |> elem(1)
+        )
+      }
     end)
+    |> elem(1)
 
     Enum.map(0..max_r, fn(r) ->
-      Enum.reduce(max_c..0, %{left: , center: , right: 0, acc_row: []}, fn(x, mem) ->
-        if x == "*" do
-          Map.update!(mem, :acc_row, &([x | &1]))
-        else
+      Enum.reduce(max_c..0, %{center: count_col(brd, r, max_c), right: 0, acc_row: []}, fn(x, mem) ->
+        left = count_col(brd, r, c-1)
 
-        end
+        Map.put(mem, :right, mem.center)
+        |> Map.put(:center, left)
+        |> Map.update!(:acc_row,
+          &([(if x == "*", do: x, else: left+mem.center+mem.right) | &1])
+        )
       end)
+      |> Map.get(:acc_row)
     end)
+  end
+
+  defp count_col(brd, r, c) do
+    (if brd[r][c-1] == "*", do: 1, else: 0)
+    + (if brd[r][c] == "*", do: 1, else: 0)
+    + (if brd[r][c+1] == "*", do: 1, else: 0)
   end
 end
